a55ac3337fe11883903663d13133bb4e2f117a8f
driller <eleshis@gmail.com>
2022-04-19 16:17:26 +0900

    update markdown text

--- source/code/dash_figure.py
+++ source/code/dash_figure.py
@@ -12,26 +12,9 @@ text = """
 ---
 
 ```
-app = dash.Dash(__name__)
-app.layout = html.Div(
-    [
-        html.Div(
-            [html.H1("見出し1")],
-            id="inner1",
-        ),
-        html.Div(
-            [
-                dcc.Markdown(text, style={"display": "inline-block"}),
-                dcc.Graph(figure=fig, style={"display": "inline-block"}),
-            ],
-            id="inner2",
-        ),
-    ],
-    id="outer",
-)
+import plotly.express as px
 
-if __name__ == "__main__":
-    app.run_server(debug=True)
+fig = px.line(x=[1, 2, 3], y=[3, 5, 2], width=400, height=400)
 ```
 
 """
